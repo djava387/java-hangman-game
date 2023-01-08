3bd7554a41010c58ce467cd1be6f2c04a30be779
djava387 <risad9020@gmail.com>
2023-01-08 15:05:22 +0000

    A class that allows the user to create a list of words and choose a random word

---
+++ src/main/java/org/example/WordList.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class WordList {
+    // Declare a list to hold the words and a random number generator
+    private List<String> words;
+    private Random random;
+
+    // Constructor to initialize the fields
+    public WordList() {
+        words = new ArrayList<String>();
+        random = new Random();
+    }
+
+    // Method to add a word to the list
+    public void addWord(String word) {
+        words.add(word);
+    }
+
+    // Method to get a random word from the list
+    public String getRandomWord() {
+        // Generate a random index
+        int index = random.nextInt(words.size());
+        // Return the word at the randomly generated index
+        return words.get(index);
+    }
+}
+
