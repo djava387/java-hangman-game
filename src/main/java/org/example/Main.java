840d9e5e1316589c12ba75da0a039fbf3c5b760d
djava387 <risad9020@gmail.com>
2023-01-09 22:40:36 +0000

    added break

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,73 +1,87 @@
 package org.example;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-// Create a new WordList
+// Initialize word list and add words
         WordList wordList = new WordList();
-// Add some words to the list
-        wordList.addWord("BATMAN");
-        wordList.addWord("JOKER");
-        wordList.addWord("MATRIX");
-        wordList.addWord("TITANIC");
-        // Get a random word from the WordList
-        String word = wordList.getRandomWord();
-// Create a new Hangman game with the chosen word and 3 lives
-        Hangman hangman = new Hangman(word, 3);
-// Create a new Player
+        wordList.addWord("apple");
+        wordList.addWord("banana");
+        wordList.addWord("orange");
+        wordList.addWord("strawberry");
+        // Initialize player
         Player player = new Player();
+        // Get player's name
+        String name = player.getName();
+        // Display greeting with player's name
+        System.out.println("Hello, " + name + "! Welcome to Hangman.");
 
-// While the game is not won or lost, keep playing
-        while (!hangman.isWon() && !hangman.isLost()) {
-            // Print the current word with blanks for unguessed letters
-            System.out.println("Word: " + hangman.getWordWithBlanks());
-            // Print the letters that have been guessed so far
-            System.out.println("Guessed Letters so far: " + hangman.lettersGuessed);
-            // Print the number of lives remaining
-            System.out.println("Lives: " + hangman.getLives());
+        while (true) {
+            // Get a random word from the word list
+            String word = wordList.getRandomWord();
+            // Initialize game with the random word and 5 lives
+            Hangman game = new Hangman(word, 3);
 
-            // Get the player's guess
-            char guess = player.getGuess();
-            // Check if the guess is correct
-            if (hangman.guessLetter(guess)) {
-                // If the guess is correct, print a message
-                player.printMessage("Correct!");
-            } else {
-                // If the guess is incorrect, print a message
-                player.printMessage("Incorrect!");
-            }
+            while (!game.isWon() && !game.isLost()) {
+                // Display the word with blanks for unguessed letters
+                System.out.println("Word: " + game.getWordWithBlanks());
+                // Display the number of lives remaining
+                System.out.println("Guessed Letters so far: " + game.lettersGuessed);
+                System.out.println("Lives remaining: " + game.getLives());
 
-            // Check the number of lives remaining and print the hangman drawing
-            if (hangman.getLives() == 2) {
-                System.out.println("   ");
-                System.out.println("   ");
-                System.out.println("   ");
-                System.out.println("   ");
-                System.out.println("   ");
-                System.out.println("   ");
-                System.out.println("_____");
-            } else if (hangman.getLives() == 1) {
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("___|___");
-            } else if (hangman.getLives() == 0) {
-                // If the player has no lives left, print a message and the hangman drawing
-                System.out.println("END OF GAME NO GUESSES LEFT");
-                System.out.println("   ____________");
-                System.out.println("   |/          |");
-                System.out.println("   |          (_)");
-                System.out.println("   |           |");
-                System.out.println("   |           | ");
-                System.out.println("   |          / \\");
-                System.out.println("   |              ");
-                System.out.println("___|___           ");
-                System.out.println("The word was " + word + " LIVES LEFT : " + hangman.getLives());
-            }
-            // If the game is won, print a message
-            if (hangman.isWon()) {
-                player.printMessage("You won!");
+                // Get player's guess
+                char guess = player.getGuess();
+                // Process the guess
+                boolean correct = game.guessLetter(guess);
+                if (correct) {
+                    System.out.println("Correct guess!");
+                } else {
+                    System.out.println("Incorrect guess!");
+                }
+                // Check the number of lives remaining and print the hangman drawing
+                if (game.getLives() == 2) {
+                    System.out.println("   ");
+                    System.out.println("   ");
+                    System.out.println("   ");
+                    System.out.println("   ");
+                    System.out.println("   ");
+                    System.out.println("   ");
+                    System.out.println("_____");
+                } else if (game.getLives() == 1) {
+                    System.out.println("   |");
+                    System.out.println("   |");
+                    System.out.println("   |");
+                    System.out.println("   |");
+                    System.out.println("   |");
+                    System.out.println("   |");
+                    System.out.println("___|___");
+                } else if (game.getLives() == 0) {
+                    // If the player has no lives left, print a message and the hangman drawing
+                    System.out.println("END OF GAME NO GUESSES LEFT");
+                    System.out.println("   ____________");
+                    System.out.println("   |/          |");
+                    System.out.println("   |          (_)");
+                    System.out.println("   |           |");
+                    System.out.println("   |           | ");
+                    System.out.println("   |          / \\");
+                    System.out.println("   |              ");
+                    System.out.println("___|___           ");
+                    System.out.println("The word was " + word);
+                }
+                // Display the final word
+                System.out.println("Word: " + game.getWordWithBlanks());
+
+                if (game.isWon()) {
+                    System.out.println("Congratulations, you won!");
+                } else {
+                    System.out.println("You lost. Better luck next time.");
+                }
+
+                // Ask the player if they want to play again
+                if (!player.wantsToPlayAgain()) {
+                    break;
+                }
             }
         }
     }
